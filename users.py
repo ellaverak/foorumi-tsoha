bd15001a36de4b49daead46d0ed071f841a55119
ellaverak <ella.korkeaaho@gmail.com>
2022-01-23 21:08:33 +0200

    Add login, logout

--- users.py
+++ users.py
@@ -6,9 +6,28 @@ from werkzeug.security import check_password_hash, generate_password_hash
 def register(username, password):
     hash_value = generate_password_hash(password)
     try:
-        sql = """INSERT INTO users (username,password) VALUES (:username,:password)"""
+        sql = "INSERT INTO users (username,password) VALUES (:username,:password)"
         db.session.execute(sql, {"username":username, "password":hash_value})
         db.session.commit()
     except:
         return False
-    return True
+    return login(username, password)
+    
+def login(username, password):
+    sql = "SELECT id, password FROM users WHERE username=:username"
+    result = db.session.execute(sql, {"username":username})
+    user = result.fetchone()
+    if not user:
+        return False
+    else:
+        if check_password_hash(user.password, password):
+            session["user_id"] = user.id
+            return True
+        else:
+            return False
+
+def logout():
+    del session["user_id"]
+
+def user_id():
+    return session.get("user_id",0)
