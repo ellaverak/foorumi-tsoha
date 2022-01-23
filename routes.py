3677940456e115ecc9adbfb117ebd15bab7e7946
ellaverak <ella.korkeaaho@gmail.com>
2022-01-23 19:03:21 +0200

    Add /register

--- routes.py
+++ routes.py
@@ -4,3 +4,17 @@ from flask import render_template, request, redirect
 @app.route("/")
 def index():
     return render_template("index.html")
+    
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    return render_template("register.html")
+    if request.method == "POST":
+        username = request.form["username"]
+        password1 = request.form["password1"]
+        password2 = request.form["password2"]
+        if password1 != password2:
+            return render_template("error.html", message="Salasanat eroavat")
+        if users.register(username, password1):
+            return redirect("/")
+        else:
+            return render_template("error.html", message="Rekisteröinti ei onnistunut")
