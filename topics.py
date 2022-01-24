bbe797b04bb5b0cd20778d99de2e436f0fed0d79
ellaverak <ella.korkeaaho@gmail.com>
2022-01-24 14:52:19 +0200

    Modify

--- topics.py
+++ topics.py
@@ -1,7 +1,12 @@
 from db import db
 
 def get_topics():
-    sql = "SELECT name FROM topics"
+    sql = "SELECT id, name FROM topics ORDER BY id DESC"
     result = db.session.execute(sql)
-    topics = result.fetchall()
-    return topics
+    return  result.fetchall()
+
+def show_topic(id):
+    sql = "SELECT name FROM topics WHERE id=:id"
+    result = db.session.execute(sql, {"id":id})
+    topic = result.fetchone()[0]
+    return topic
