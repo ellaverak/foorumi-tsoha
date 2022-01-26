2bfe16112274d24e3d14d7753b8c1b552692c838
ellaverak <ella.korkeaaho@gmail.com>
2022-01-26 22:59:28 +0200

    Modify

--- topics.py
+++ topics.py
@@ -1,8 +1,7 @@
-import os
 from db import db
 
 def get_topics():
-    sql = "SELECT T.id, T.name, COUNT(H.*) FROM threads H, topics T WHERE T.id = H.topic_id GROUP BY T.id ORDER BY T.id ASC"
+    sql = "SELECT T.id, T.name, COUNT(H.*) FROM topics T LEFT JOIN threads H ON T.id = H.topic_id GROUP BY T.id ORDER BY T.id ASC"
     result = db.session.execute(sql)
     return  result.fetchall()
 
