df92c5bdf6af99d02ca4b66855982d2431b55123
ellaverak <ella.korkeaaho@gmail.com>
2022-01-28 22:41:46 +0200

    Add new_thread(), create_new_thread(), reply()

--- routes.py
+++ routes.py
@@ -1,8 +1,6 @@
 from app import app
 from flask import render_template, request, redirect
-import users
-import topics
-import thread
+import users, topics, thread
 
 @app.route("/")
 def index():
@@ -43,10 +41,33 @@ def logout():
 @app.route("/topics<int:id>")
 def show_topic(id):
     top = list(topics.show_topic(id))
-    return render_template("topic.html", topic=top)
+    return render_template("topic.html", topic=top, topic_id=id)
     
 @app.route("/thread<int:id>")
 def show_thread(id):
     thre = list(thread.show_thread(id))
-    return render_template("thread.html", thread=thre)
+    return render_template("thread.html", thread=thre, thread_id=id)
+    
+@app.route("/new<int:id>")
+def new_thread(id):
+    return render_template("new_thread.html", topic_id=id)
+
+@app.route("/create_new", methods=["POST"])
+def create_new_thread():
+    title = request.form["title"]
+    op_content = request.form["op_content"]
+    topic_id = request.form["topic_id"]
+    if thread.create_thread(title, op_content, topic_id):
+        return redirect("/")
+    else:
+        return render_template("error.html", message="Langan luominen ei onnistunut")
+    
+@app.route("/reply", methods=["POST"])
+def reply():
+    content = request.form["content"]
+    thread_id = request.form["thread_id"]
+    if thread.reply(content, thread_id):
+        return redirect("/thread"+str(thread_id))
+    else:
+        return render_template("error.html", message="Viestin lähetys ei onnistunut")
     
