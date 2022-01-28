7d993910345a3e674d5f8fc640c9a08e35a75bca
ellaverak <ella.korkeaaho@gmail.com>
2022-01-29 01:17:17 +0200

    Modify

--- users.py
+++ users.py
@@ -1,3 +1,4 @@
+import os
 from db import db
 from flask import session
 from werkzeug.security import check_password_hash, generate_password_hash
@@ -5,7 +6,7 @@ from werkzeug.security import check_password_hash, generate_password_hash
 def register(username, password):
     hash_value = generate_password_hash(password)
     try:
-        sql = "INSERT INTO users (username,password) VALUES (:username,:password)"
+        sql = "INSERT INTO users (username,password, role) VALUES (:username,:password, 0)"
         db.session.execute(sql, {"username":username, "password":hash_value})
         db.session.commit()
     except:
@@ -13,7 +14,7 @@ def register(username, password):
     return login(username, password)
     
 def login(username, password):
-    sql = "SELECT id, password FROM users WHERE username=:username"
+    sql = "SELECT id, password, role FROM users WHERE username=:username"
     result = db.session.execute(sql, {"username":username})
     user = result.fetchone()
     if not user:
@@ -21,12 +22,17 @@ def login(username, password):
     else:
         if check_password_hash(user.password, password):
             session["user_id"] = user.id
+            session["user_role"] = user.role
             return True
         else:
             return False
 
 def logout():
     del session["user_id"]
+    del session["user_role"]
 
 def user_id():
     return session.get("user_id",0)
+    
+def require_role(role):
+    return session.get("user_role", 0)
