e11580da1d1bae77ce3aad79c8c92e9c6be2a0d2
ellaverak <ella.korkeaaho@gmail.com>
2022-02-05 17:36:55 +0200

    Modify show_topic()

--- topics.py
+++ topics.py
@@ -7,12 +7,12 @@ def get_topics():
     return  result.fetchall()
 
 def get_secret_topics():
-    sql = "SELECT A.id, A.name, (SELECT COUNT(H.*) FROM topics B LEFT JOIN threads H ON B.id = H.topic_id WHERE B.id = A.id), (SELECT COUNT(R.*) FROM threads H LEFT JOIN replys R ON H.id = R.thread_id WHERE H.topic_id = A.id), (SELECT MIN(R.sent_at) FROM threads H LEFT JOIN replys R ON H.id = R.thread_id WHERE H.topic_id = A.id) FROM topics A WHERE secret=1"
+    sql = "SELECT A.id, A.name, (SELECT COUNT(H.*) FROM topics B LEFT JOIN threads H ON B.id = H.topic_id WHERE B.id = A.id), (SELECT COUNT(R.*) FROM threads H LEFT JOIN replys R ON H.id = R.thread_id WHERE H.topic_id = A.id), (SELECT MIN(R.sent_at) FROM threads H LEFT JOIN replys R ON H.id = R.thread_id WHERE H.topic_id = A.id), A.user_id FROM topics A WHERE secret=1"
     result = db.session.execute(sql)
     return  result.fetchall()
 
 def show_topic(id):
-    sql = "SELECT T.name, H.id, H.title, H.sent_at, H.user_id FROM topics T LEFT JOIN threads H  ON T.id = H.topic_id WHERE T.id=:id"
+    sql = "SELECT T.name, H.id, H.title, H.sent_at, H.user_id, T.secret FROM topics T LEFT JOIN threads H  ON T.id = H.topic_id WHERE T.id=:id"
     result = db.session.execute(sql, {"id":id})
     return result.fetchall()
     
@@ -32,11 +32,11 @@ def create_topic(name):
     return True
 
 def create_secret_topic(name, choises):
-    sql = "INSERT INTO topics (name, secret) VALUES (:name, 1)"
-    db.session.execute(sql, {"name":name})
+    user_id = session.get("user_id", 0)
+    sql = "INSERT INTO topics (name, secret, user_id) VALUES (:name, 1, :user_id)"
+    db.session.execute(sql, {"name":name, "user_id":user_id})
     sql = "SELECT id FROM topics WHERE name=:name"
     topic_id = db.session.execute(sql, {"name":name}).fetchone()[0]
-    user_id = session.get("user_id", 0)
     for line in choises.split("\n"):
         line = line.replace("\r", "")
         
