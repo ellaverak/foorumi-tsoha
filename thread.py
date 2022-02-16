9593754f6739ad7c2566990e2c32a78b00dbc187
ellaverak <ella.korkeaaho@gmail.com>
2022-02-17 00:49:30 +0200

    Improved code style

--- thread.py
+++ thread.py
@@ -5,7 +5,7 @@ from flask import session
 def show_thread(id):
     sql = "SELECT H.title, H.op_content, (SELECT U.username FROM users U WHERE U.id = H.user_id), R.id, R.content, R.user_id, (SELECT U.username FROM users U WHERE U.id = R.user_id), R.sent_at, H.id, H.user_id FROM threads H LEFT JOIN replies R ON H.id = R.thread_id WHERE H.id=:id"
     result = db.session.execute(sql, {"id":id})
-    return  result.fetchall()
+    return  list(result.fetchall())
 
 def create_thread(title, op_content, topic_id):
     user_id = users.user_id()
@@ -17,9 +17,12 @@ def create_thread(title, op_content, topic_id):
     return True, new
     
 def delete_thread(id):
-    sql = "DELETE FROM threads WHERE id=:id RETURNING topic_id"
-    topic_id = db.session.execute(sql, {"id":id}).fetchone()[0]
-    db.session.commit()
+    try:
+        sql = "DELETE FROM threads WHERE id=:id RETURNING topic_id"
+        topic_id = db.session.execute(sql, {"id":id}).fetchone()[0]
+        db.session.commit()
+    except:
+        return False
     return True, topic_id
 
 def edit_thread(title, op_content, thread_id):
