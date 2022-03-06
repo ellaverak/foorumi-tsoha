410a5e25058ae35411b2c8798a2165a961eb3bdf
ellaverak <ella.korkeaaho@gmail.com>
2022-03-06 23:17:34 +0200

    Improve code format

--- thread.py
+++ thread.py
@@ -1,7 +1,6 @@
-from db import db
-import users
 import textwrap
 from flask import session
+from db import db
 
 
 def show_thread(id):
@@ -9,24 +8,27 @@ def show_thread(id):
              FROM threads H LEFT JOIN users U ON U.id = H.user_id WHERE H.id=:id"""
     result = db.session.execute(sql, {"id": id})
     result1 = list(result.fetchall())
-    sql = """SELECT R.id, R.content, R.user_id, (SELECT U.username FROM users U WHERE U.id = R.user_id), 
-             R.sent_at, H.id FROM threads H LEFT JOIN replies R ON H.id = R.thread_id WHERE H.id=:id ORDER BY R.sent_at"""
+    sql = """SELECT R.id, R.content, R.user_id, (SELECT U.username
+             FROM users U WHERE U.id = R.user_id),
+             R.sent_at, H.id FROM threads H LEFT JOIN replies R
+             ON H.id = R.thread_id WHERE H.id=:id ORDER BY R.sent_at"""
     result = db.session.execute(sql, {"id": id})
     result2 = list(result.fetchall())
     return [result1, result2]
 
 
 def create_thread(title, op_content, topic_id):
-    user_id = users.user_id()
-    if user_id == 0:
+    if session.get("user_id", 0) == 0:
         return False, False
+
+    user_id = session.get("user_id", 0)
 
     op_content = "\n".join(textwrap.wrap(op_content, width=78))
 
-    sql = """INSERT INTO threads (title, op_content, topic_id, user_id, sent_at) 
+    sql = """INSERT INTO threads (title, op_content, topic_id, user_id, sent_at)
              VALUES (:title, :op_content, :topic_id, :user_id, NOW()) RETURNING id"""
     new = db.session.execute(sql, {"title": title, "op_content": op_content,
-                             "topic_id": topic_id, "user_id": user_id}).fetchone()[0]
+                                   "topic_id": topic_id, "user_id": user_id}).fetchone()[0]
     db.session.commit()
     return True, new
 
@@ -42,8 +44,7 @@ def delete_thread(id):
 
 
 def edit_thread(title, op_content, thread_id):
-    user_id = users.user_id()
-    if user_id == 0:
+    if session.get("user_id", 0) == 0:
         return False
 
     op_content = "\n".join(textwrap.wrap(op_content, width=78))
