b25ab5129642b509cd5145a65535686f0d249397
ellaverak <ella.korkeaaho@gmail.com>
2022-03-06 19:56:52 +0200

    Format code

--- routes.py
+++ routes.py
@@ -1,27 +1,35 @@
+from os import abort
 from app import app
 from flask import render_template, request, redirect, url_for, session
-import users, topics, thread, reply
+import users
+import topics
+import thread
+import reply
+
 
 @app.context_processor
 def inject_users():
     all_users = users.get_users()
     return dict(all_users=all_users)
 
+
 @app.context_processor
 def inject_topics():
     topic_list = topics.get_list()
     return dict(topic_list=topic_list)
+
 
 @app.route("/")
 def index():
     topics_ = topics.get_topics()
     return render_template("index.html", topics=topics_)
 
+
 @app.route("/register", methods=["POST", "GET"])
 def register():
     if request.method == "GET":
         return render_template("register.html")
-        
+
     if request.method == "POST":
         username = request.form["username"]
         password1 = request.form["password1"]
@@ -37,6 +45,7 @@ def register():
         else:
             return render_template("error.html", message="Käyttäjänimi on jo käytössä")
 
+
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
@@ -47,10 +56,12 @@ def login():
         else:
             return render_template("error.html", message="Väärä tunnus tai salasana")
 
+
 @app.route("/logout")
 def logout():
     users.logout()
     return redirect(request.referrer)
+
 
 @app.route("/topics<int:id>")
 def show_topic(id):
@@ -61,10 +72,11 @@ def show_topic(id):
         return render_template("topic.html", topic=topic, topic_id=id, users=users_)
     else:
         if users.access(id):
-           return render_template("topic.html", topic=topic, topic_id=id, users=users_)
+            return render_template("topic.html", topic=topic, topic_id=id, users=users_)
         else:
-           return render_template("error.html", message="Sinulla ei ole pääsyä alueelle")
-    
+            return render_template("error.html", message="Sinulla ei ole pääsyä alueelle")
+
+
 @app.route("/thread<int:id>")
 def show_thread(id):
     thread_ = thread.show_thread(id)[0]
@@ -78,11 +90,13 @@ def show_thread(id):
             return render_template("thread.html", thread=thread_, replies=replies_, thread_id=id, topic_info=topic_info)
         else:
             return render_template("error.html", message="Sinulla ei ole pääsyä ketjuun")
-    
+
+
 @app.route("/new<int:id>")
 def new_thread(id):
     topic_info = topics.get_info_topic(id)
     return render_template("new_thread.html", topic_id=id, topic_info=topic_info)
+
 
 @app.route("/create_new", methods=["POST"])
 def create_new_thread():
@@ -100,7 +114,8 @@ def create_new_thread():
         return redirect("/thread"+str(result[1]))
     else:
         return render_template("error.html", message="Ketjun luominen ei onnistunut")
-    
+
+
 @app.route("/reply", methods=["POST"])
 def reply_to():
     if session["csrf_token"] != request.form["csrf_token"]:
@@ -108,12 +123,13 @@ def reply_to():
     content = request.form["content"]
     thread_id = request.form["thread_id"]
     if len(content) == 0:
-            return render_template("error.html", message="Kirjoita viesti")
+        return render_template("error.html", message="Kirjoita viesti")
     if reply.leave_reply(content, thread_id):
         return redirect("/thread"+str(thread_id))
     else:
         return render_template("error.html", message="Viestin lähetys ei onnistunut")
-        
+
+
 @app.route("/delete_reply<int:id>")
 def delete_reply(id):
     result = reply.delete_reply(id)
@@ -121,8 +137,8 @@ def delete_reply(id):
         return redirect("/thread"+str(result[1]))
     else:
         return render_template("error.html", message="Viestin poistaminen ei onnistunut")
-        
-    
+
+
 @app.route("/edit_reply", methods=["POST"])
 def edit_reply():
     if session["csrf_token"] != request.form["csrf_token"]:
@@ -131,12 +147,13 @@ def edit_reply():
     reply_id = request.form["reply_id"]
     thread_id = request.form["thread_id"]
     if len(content) == 0:
-            return render_template("error.html", message="Kirjoita viesti")
+        return render_template("error.html", message="Kirjoita viesti")
     if reply.edit_reply(content, reply_id):
         return redirect("/thread"+str(thread_id))
     else:
         return render_template("error.html", message="Viestin muokkaus ei onnistunut")
-        
+
+
 @app.route("/delete_thread<int:id>")
 def delete_thread(id):
     result = thread.delete_thread(id)
@@ -144,6 +161,7 @@ def delete_thread(id):
         return redirect("/topics"+str(result[1]))
     else:
         return render_template("error.html", message="Ketjun poistaminen ei onnistunut")
+
 
 @app.route("/edit_thread", methods=["POST"])
 def edit_thread():
@@ -160,14 +178,16 @@ def edit_thread():
         return redirect("/thread"+str(thread_id))
     else:
         return render_template("error.html", message="Ketjun muokkaus ei onnistunut")
-    
+
+
 @app.route("/topic_options")
 def topic_options():
     users_ = users.get_users()
     topics_ = topics.get_list()
     return render_template("topic_options.html", users=users_, topics=topics_)
-    
-@app.route("/delete_topic", methods=["POST"])   
+
+
+@app.route("/delete_topic", methods=["POST"])
 def delete_topic():
     if session["csrf_token"] != request.form["csrf_token"]:
         abort(403)
@@ -178,7 +198,8 @@ def delete_topic():
         return redirect("/")
     else:
         return render_template("error.html", message="Alueen poisto ei onnistunut. Syy: Virhe tai perusalueen poistokäsky.")
-    
+
+
 @app.route("/create_topic", methods=["POST"])
 def create_topic():
     if session["csrf_token"] != request.form["csrf_token"]:
@@ -190,13 +211,15 @@ def create_topic():
     if result[0]:
         return redirect("/topics"+str(result[1]))
     else:
-        return render_template("error.html", message="Alueen luonti ei onnistunut")        
+        return render_template("error.html", message="Alueen luonti ei onnistunut")
+
 
 @app.route("/secret_topics")
 def secret_topics():
     s_topic = topics.get_secret_topics()
     users_ = users.get_users()
     return render_template("secret_topics.html", s_topics=s_topic, users=users_)
+
 
 @app.route("/create_secret_topic", methods=["POST"])
 def create_secret():
@@ -210,7 +233,8 @@ def create_secret():
     if result[0]:
         return redirect("/topics"+str(result[1]))
     else:
-        return render_template("error.html", message="Salaisen alueen luonti ei onnistunut")  
+        return render_template("error.html", message="Salaisen alueen luonti ei onnistunut")
+
 
 @app.route("/result", methods=["POST"])
 def result():
@@ -219,6 +243,7 @@ def result():
     query = request.form["query"]
     replies = reply.search(query)
     return render_template("result.html", replies=replies)
+
 
 @app.route("/add_secret_users", methods=["POST"])
 def add_secret():
@@ -230,4 +255,3 @@ def add_secret():
         return redirect("/topics"+str(topic_id))
     else:
         return render_template("error.html", message="Käyttäjien lisääminen ei onnistunut")
-        
